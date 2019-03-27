2f2071e95967348fb980157ae12a9f0e39b2502f
Matthew Vowell <mvowell@daltonstate.edu>
2019-03-27 13:28:45 -0400

    Removed some files

--- java/IDictionaryStorage.java
+++
@@ -1,9 +0,0 @@
-public interface IDictionaryStorage {
-   void addWord(String word);
-   void addWord(String firstWord, String secondWord);
-   String[] getWords(String firstWord);
-   String[] allWords();
-   double getPercentage(String firstWord, String secondWord);
-   boolean isKnownWord(String word);
-   boolean isKnownWord(String firstWord, String secondWord);
-}
